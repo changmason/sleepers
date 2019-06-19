ca5aa7a6d38fb1e43ee17b7289143633026bb955
Mason <changmason@gmail.com>
2019-06-19 22:51:27 +0800

    Contruct Friendship model for follower and following friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   has_many :sleeps
 
+  # follower friends
+  has_many :followerships, class_name: 'Friendship', foreign_key: 'following_id'
+  has_many :followers, through: :followerships, source: :follower
+
+  # following friends
+  has_many :followingships, class_name: 'Friendship', foreign_key: 'follower_id'
+  has_many :followings, through: :followingships, source: :following
+
   validates :email, presence: true, uniqueness: true
 
   before_create :generate_api_key
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,30 @@ RSpec.describe User, type: :model do
   let!(:user) { create(:user) }
 
   it { should have_many :sleeps }
+  it { should have_many :followerships }
+  it { should have_many :followers }
+  it { should have_many :followingships }
+  it { should have_many :followings }
+
   it { should validate_presence_of :email }
   it { should validate_uniqueness_of :email }
 
+  context 'add followers' do
+    let!(:follower) { create(:user, name: 'Follower') }
+
+    it 'becomes following of the follower' do
+      user.followers << follower
+      expect(follower.followings).to include(user)
+    end
+  end
+
+  context 'add followings' do
+    let!(:following) { create(:user, name: 'Following') }
+
+    it 'becomes follower of the following' do
+      user.followings << following
+      expect(following.followers).to include(user)
+    end
+  end
 
 end
